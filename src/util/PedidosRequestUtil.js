424e612c2ac5dc9403e946f24fb3014542b1fc87
Vinicius Souza <viniciuspodi@gmail.com>
2020-07-23 00:28:48 -0400

    Corrigindo link da API

--- src/util/PedidosRequestUtil.js
+++ src/util/PedidosRequestUtil.js
@@ -1,5 +1,5 @@
-// const API_URL = "https://high-service-api.herokuapp.com/transacao";
-const API_URL = "http://localhost:3000/transacao";
+const API_URL = "https://high-service-api.herokuapp.com/transacao";
+// const API_URL = "http://localhost:3000/transacao";
 import axios from "axios";
 
 export default {
